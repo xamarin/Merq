8c643c45529817138a7e1663d4004d84dc2b48c7
Daniel Cazzulino <daniel@cazzulino.com>
2023-02-02 16:07:42 +0000

    Clean dependency on Merq packages, simplify source repository (#21)
    * Remove unused installer projects
    
    We no longer generate or need WIX setup projects (haven't had for a while).
    
    Remove all that unnecessary complexity from the repository.
    
    * Remove source for core assemblies, remove unnecessary XVSSDK
    
    This cleans up substantially the source tree, and keeps only the things we need to bundle for install.
    
    * Ensure Merq package assemblies are signed, simplify version attr.
    
    * Bump to latest 1.3.0 Merq stable
    
    Remove tests since those are now built-in the upstream repo.
    
    Integration tests pass green, of course.
    
    * Change from Microsoft to 3PartySHA2 as requested

--- src/Vsix/Merq.Vsix.IntegrationTests/ComponentsSpec.cs
+++ src/Vsix/Merq.Vsix.IntegrationTests/ComponentsSpec.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.ComponentModel.Composition.Diagnostics;
 using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -21,38 +15,12 @@ namespace Merq
 			this.output = output;
 		}
 
-		[InlineData(typeof(ICommandBus))]
-		[InlineData(typeof(IEventStream))]
-		[VsixTheory]
-		public void when_retrieving_exports_then_reports_duplicate_services(Type serviceType)
-		{
-			var componentModel = GlobalServices.GetService<SComponentModel, IComponentModel>();
-			var contractName = AttributedModelServices.GetContractName(serviceType);
-			var components = componentModel.DefaultExportProvider
-				.GetExports<object, IDictionary<string, object>>(contractName)
-				.ToArray();
-
-			if (components.Length != 1)
-			{
-				var info = new CompositionInfo(componentModel.DefaultCatalog, componentModel.DefaultExportProvider);
-				var log = Path.GetTempFileName();
-				using (var writer = new StreamWriter(log))
-				{
-					CompositionInfoTextFormatter.Write(info, writer);
-					writer.Flush();
-				}
-
-				output.WriteLine(log);
-				// Process.Start(new ProcessStartInfo("notepad", log) { UseShellExecute = true });
-
-				Assert.False(true, $"Expected only one component of {serviceType.Name}. Composition log at {log}");
-			}
-		}
-
-		[VsixFact]
+		[VsixFact(RunOnUIThread = true)]
 		public void when_subscribing_and_pushing_events_then_succeeds()
 		{
-			var stream = GlobalServices.GetService<SComponentModel, IComponentModel>().GetService<IEventStream>();
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			var stream = ServiceProvider.GlobalProvider.GetService<SComponentModel, IComponentModel>().GetService<IEventStream>();
 			var expected = new FooEvent();
 
 			FooEvent actual = null;
@@ -64,10 +32,12 @@ namespace Merq
 			Assert.Same(expected, actual);
 		}
 
-		[VsixFact]
+		[VsixFact(RunOnUIThread = true)]
 		public void when_querying_command_bus_for_handler_then_succeeds()
 		{
-			var commands = GlobalServices.GetService<SComponentModel, IComponentModel>().GetService<ICommandBus>();
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			var commands = ServiceProvider.GlobalProvider.GetService<SComponentModel, IComponentModel>().GetService<ICommandBus>();
 
 			Assert.False(commands.CanHandle(new FooCommand()));
 			Assert.False(commands.CanHandle<FooAsyncCommand>());
